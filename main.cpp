6b5657925012526860988ff2edddc87476e3fc16
Dev-Omid-s <omiisa4214@gmail.com>
2025-10-16 22:38:10 +0330

    Debug & Get last Name .cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 
 int main () {
 
-    std::cout << " hi , Please Enter Your Name :" << std::endl ; 
-    std::string Name ;
-    std::cin >> Name ;
+    std::cout << " hi , Please Enter Your First Name :" << std::endl ; 
+    std::string fName ;
+    std::cin >> fName ;
 
+
+    std::cout << " Welcome \"<< fName <<"\" Please Enter Your Last Name" << std::endl ;
+    std::string lName ;
+    std::cin >> lName ;
 
 
     return 0 ; 

f7b4a9626d85f3c0c1772eff25d5f730feb82b68
Mr.Omid_Saidiyan <omiisa4214@gmail.com>
2025-10-16 22:31:07 +0330

    first Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main () {
+
+    std::cout << " hi , Please Enter Your Name :" << std::endl ; 
+    std::string Name ;
+    std::cin >> Name ;
+
+
+
+    return 0 ; 
+}
